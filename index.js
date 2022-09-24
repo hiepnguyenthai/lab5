1ce33c2227837ea6c50bbe32404f8c5832e96415
hiepnguyenthai <HiepNTGCH200524@fpt.edu.vn>
2022-09-24 17:29:23 +0700

    ok

--- index.js
+++ index.js
@@ -16,21 +16,22 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  var name = "Cloud Computing"; //string
-  var code = 1644; //int
-  //pass dữ liêu từ back-end sang front-end
+  let name = "Cloud Computing"; //string
+  let code = 1644; //int
+  //pass dữ liệu từ back-end sang front-end
   //cú pháp: name (data) : value (data)
   //gửi dữ liệu là value và nhận với tên là name
-  res.render("index", {name: name, ma: code, ky: "Fall 2022"});
+  res.render("index", { name: name, ma: code, ky: "Fall 2022" });
 });
 
 app.get("/mobile", (req, res) => {
-  var list = ["IPhone", "SamSung", "Oppo", "Huewai"]
-  res.render("mobile", {mobiles: list});
+  let list = ["iPhone", "Samsung", "Oppo", "Huawei"];
+  res.render("mobile", { mobiles: list });
 });
 
 app.get("/laptop", (req, res) => {
-  res.render("laptop");
+  let list = ["Macbook", "Dell", "HP", "Asus", "LG"];
+  res.render("laptop", { laptops: list });
 });
 
 //listen port để chạy server
